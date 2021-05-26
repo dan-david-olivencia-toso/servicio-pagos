5b26f1b831350149c3abe896a21ccb3d474953de
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-05-26 03:40:26 -0300

    Agregada implementación de clases de dominio

--- src/main/java/com/dan/dot/pagos/domain/Pago.java
+++ src/main/java/com/dan/dot/pagos/domain/Pago.java
@@ -1,4 +1,42 @@
 package com.dan.dot.pagos.domain;
 
+import java.time.Instant;
+
 public class Pago {
+    private Integer id;
+    private Cliente cliente;
+    private Instant fecha;
+    private FormaPago forma;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Instant getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Instant fecha) {
+        this.fecha = fecha;
+    }
+
+    public FormaPago getForma() {
+        return forma;
+    }
+
+    public void setForma(FormaPago forma) {
+        this.forma = forma;
+    }
 }
--- src/main/java/com/dan/dot/pagos/domain/Transferencia.java
+++ src/main/java/com/dan/dot/pagos/domain/Transferencia.java
@@ -1,4 +1,26 @@
 package com.dan.dot.pagos.domain;
 
 public class Transferencia extends FormaPago {
+    private String cbuOrigen;
+    private String cbuDestino;
+    private Long codigoTransferencia;
+
+    public Long getCodigoTransferencia() {
+        return codigoTransferencia;
+    }
+    public void setCodigoTransferencia(Long codigoTransferencia) {
+        this.codigoTransferencia = codigoTransferencia;
+    }
+    public String getCbuOrigen() {
+        return cbuOrigen;
+    }
+    public void setCbuOrigen(String cbuOrigen) {
+        this.cbuOrigen = cbuOrigen;
+    }
+    public String getCbuDestino() {
+        return cbuDestino;
+    }
+    public void setCbuDestino(String cbuDestino) {
+        this.cbuDestino = cbuDestino;
+    }
 }
